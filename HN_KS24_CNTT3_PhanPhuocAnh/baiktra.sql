cd4e58a101b8882cf45808342fc7e2dd312e6201
Phanh2910 <phanphuocanh97@gmail.com>
2025-12-30 07:52:01 +0700

    Add files via upload

---
+++ HN_KS24_CNTT3_PhanPhuocAnh/baiktra.sql
@@ -0,0 +1,63 @@
+create database test;
+use test;
+
+create table Student(
+    students_id varchar(15) primary key,
+    students_name varchar(50) not null,
+    students_email varchar(50) unique not null,
+	students_phoneNum varchar(15) not null unique
+);
+
+create table Courses(
+     course_id varchar(15) primary key not null unique,
+     course_name varchar(50) not null,
+     course_credit int not null check (course_credit > 0)
+);
+
+create table Enrollments(
+      students_id varchar(15),
+      course_id varchar(15),
+	  grade decimal(4,2) not null,
+      primary key(students_id, course_id),
+      
+      foreign key(students_id)
+		references Student(students_id),
+	  foreign key(course_id)
+        references Course(course_id)
+        
+);
+
+insert into StudentS(students_id, students_name, students_email, students_phoneNum)
+values
+("01","Nguyen Van A","Eggx@gmail.com","0931910112"),
+("02","Nguyen Van A","Eggx@gmail.com","0192092333"),
+("03","Nguyen Van A","Eggx@gmail.com","0931910111"),
+("04","Nguyen Van A","Eggx@gmail.com","0982828288"),
+("05","Nguyen Van A","Eggx@gmail.com","0988822212");
+
+insert into Courses(course_id, course_name,course_credit)
+values 
+(1,"co so du lieu",3),
+(2,"lap trinh huong doi tuong",4),
+(3,"lap trinh java",3),
+(4,"toan cao cap",4),
+(5,"triet hoc dai cuong",3);
+
+insert into Enrollments(students_id,course_id, grade)
+ values
+(1, 1, 8.50),
+(1, 2, 7.00),
+(2, 4, 9.00),
+(3, 3, 6.75),
+(4, 5, 8.25);
+
+update Enrollments
+set grade = 9.00
+where student_id = "2" and course_id = "4";
+
+select student_name,student_email,student_phoneNum
+from student
+
+
+
+
